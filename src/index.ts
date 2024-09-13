411e51e6870a4eedcc309994e0ef7ecc881fa4bf
fanman <ejfamanas@gmail.com>
2024-09-13 19:51:20 +0100

    test env boilerplate

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import myFunction from "./utils/myFunction";
+
+console.log("Hello World");
+
+
+
+console.log(myFunction(1, 2));
---
+++ src/utils/myFunction.ts
@@ -0,0 +1,3 @@
+export default function myFunction(x: number, y: number): number {
+    return x + y;
+}
