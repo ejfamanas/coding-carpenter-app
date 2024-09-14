3e6a742d618dec36372a91faac152425b1039e40
fanman <ejfamanas@gmail.com>
2024-09-14 18:49:17 +0100

    dependency update

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-import myFunction from "./utils/myFunction";
-
-console.log("Hello World");
-
-
-
-console.log(myFunction(1, 2));
--- src/utils/myFunction.ts
+++
@@ -1,3 +0,0 @@
-export default function myFunction(x: number, y: number): number {
-    return x + y;
-}
